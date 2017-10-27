d157a75997f8e1a2e968aa6f4cca724d38d1d3af
nachotp <thenachotp@gmail.com>
2017-10-27 20:39:00 -0300

    Mazo de cartas generado

--- Tarea 2/main.cpp
+++ Tarea 2/main.cpp
@@ -9,12 +9,21 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   vector<carta> placeholder;
   stack<carta> mazo;
+
   generarCartasNumericas(&placeholder);
   generarCartasExtra(&placeholder);
   random_shuffle(placeholder.begin(),placeholder.end());
-  cout << placeholder.size() << endl;
+
   for (size_t i = 0; i < placeholder.size(); i++) {
-    placeholder[i].imprimir();
+    mazo.push(placeholder[i]);
+  }
+
+  placeholder.clear();
+
+  while (!mazo.empty()) {
+    mazo.top().imprimir();
+    cout << endl;
+    mazo.pop();
   }
   return 0;
 }
