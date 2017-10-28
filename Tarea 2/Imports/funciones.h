0138c743c8547b41c9d08018c3da577ce14381ac
nachotp <thenachotp@gmail.com>
2017-10-28 04:29:20 -0300

    Esqueleto de manejo de turnos

--- Tarea 2/Imports/funciones.h
+++ Tarea 2/Imports/funciones.h
@@ -10,16 +10,29 @@ using namespace std;
 class carta {
 public:
   string valor;
-  char color;
+  string color;
   string accion;
 
   carta(string val, char col){
     valor = val;
-    color = col;
+    if (col == 'A') {
+      color = "Amarillo";
+    } else if (col == 'R'){
+      color = "Rojo";
+    } else if (col == 'Z'){
+      color = "Azul";
+    } else {
+      color = "Verde";
+    }
+
+  }
+  carta(){
+    valor = "-1";
+    color = "-";
   }
 
   void imprimir(){
-    cout << valor << " " << color << '\t';
+    cout << "[" << valor << " " << color << "]\t";
   }
 };
 
@@ -27,8 +40,15 @@ class jugador {
 public:
   vector<carta> mano;
   int numeroJugador;
-  jugador(){
-    cout << "Jugador "<< numeroJugador <<" creado en " << getpid() << endl;
+  void robar(vector<carta> *mazo){
+    mano.push_back(mazo->back());
+    mazo->pop_back();
+  }
+  void mostrarMano(){
+    for (size_t i = 0; i < mano.size(); i++) {
+      mano[i].imprimir();
+    }
+    cout << endl;
   }
 };
 
@@ -36,10 +56,10 @@ class turnHandler {
 public:
   int turnoDe;
   turnHandler(){
-    turnoDe = 1;
+    turnoDe = 0;
   }
   int siguenteTurno(int n){
-    turnoDe = 1 + ((turnoDe + n) & 4);
+    turnoDe = ((turnoDe + n) & 4);
     return turnoDe;
   }
 };
--- Tarea 2/main.cpp
+++ Tarea 2/main.cpp
@@ -5,6 +5,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/mman.h>
+#include <sys/wait.h>
+#include <time.h>
 #include "Imports/funciones.h"
 
 using namespace std;
@@ -12,47 +14,59 @@ using namespace std;
 int main(int argc, char const *argv[]) {
   // Generar cartas y stack
   vector<carta> *mazo = (vector<carta>*)mmap(NULL, sizeof(stack<carta>), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
+  carta *upperCard = (carta*)mmap(NULL, sizeof(carta), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
   mazo->reserve(108);
   generarCartasNumericas(mazo);
   generarCartasExtra(mazo);
+  srand(time(0));
   random_shuffle(mazo->begin(),mazo->end());
-
-
+  upperCard = &(mazo->back());
+  mazo->pop_back();
   // creacion de procesos jugadores y piping
-  turnHandler coordinador;
-  int pipes[4][2];
+  turnHandler *coordinador = (turnHandler*)mmap(NULL, sizeof(turnHandler), PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);;
+  int pipes[4];
   int turnoDe = 1;
-  bool fin = true;
+  bool fin = false;
   jugador player;
   pid_t pid;
-
-  for (size_t i = 4; i < 1; i++) {
+  pipe(pipes);
+  for (size_t i = 0; i < 4; i++){
+    close(pipes[i]);
+    write(pipes[i],-1,1);
+  }
+  write(pipes[0],1,1);
+  for (size_t i = 3; i > 0; i--) {
     pid = fork();
-    pipe(pipes[i]);
     if (pid == 0){
-      player.numeroJugador = i+1;
+      player.numeroJugador = i;
       cout << "Jugador creado: " << player.numeroJugador << endl;
       break;
     }
   }
+  if (pid > 0) {
+    player.numeroJugador = 0;
+    cout << "Jugador creado: " << player.numeroJugador << endl;
+  }
 
-  if (pid > 0) player.numeroJugador = 1;
+  for (size_t i = 0; i < 7; i++) {
+    player.robar(mazo);
+  }
 
+  int pos = player.numeroJugador;
+  cout << "jugador " << pos << " ya robó dejando " << mazo->size() << " cartas restantes" << endl;
   while (!fin) {
-    //codigo de coordinador
-    if (pid > 0) {
-      if (turnoDe < 0) {
-
-
-      }
-    // codigo de jugadores
+    for (size_t i = 0; i < 4; i++) close(pipes[pos]);
+    if (turnoDe == player.numeroJugador) {
+      cout << "turno de jugador " << turnoDe << ": " << endl;
+      cout << "Carta superior: ";
+      upperCard->imprimir();
+      cout << endl << "tienes: ";
+      player.mostrarMano();
+      break;
     } else {
-      cout << &mazo[0] << endl;
-      if (turnoDe == player.numeroJugador) {
-        cout << "Turno de jugador " << player.numeroJugador << endl;
-        break;
-      }
+
     }
+    break;
   }
 
 
