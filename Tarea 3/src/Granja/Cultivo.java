23a3d5fdb551ea9ce5a28a183144a49fc74f5a14
nachotp <thenachotp@gmail.com>
2017-12-02 13:11:28 -0300

    Cultivos crecen a rate fijo

--- Tarea 3/src/Granja/Cultivo.java
+++ Tarea 3/src/Granja/Cultivo.java
@@ -1,9 +1,39 @@
 class Cultivo extends Thread {
-      public Cultivo()
-      {
-      }
+  private String nombre;
+  private int tasa;
+  private int peso;
+  private int costo;
+  private int i;
+  public Cultivo(String name, int rate, int precio){
+    this.nombre = name;
+    this.tasa = rate;
+    this.costo = precio;
+    this.peso = 0;
+    this.i = 0;
+  }
 
-     public void run()
-     {
-     }
- }
+  public String getNombre() {
+    return nombre;
+  }
+
+  public String getData(){
+    return nombre + " ("+Integer.toString(peso)+" Kg, $"+Integer.toString(costo)+") ";
+  }
+
+  public int tLeft(){
+    return i;
+  }
+
+  public void run(){
+    System.out.println(nombre + " plantado!");
+    while (peso < 20){
+      for(i = 0; i < tasa; i++){
+        try {
+          Thread.sleep(1000);
+        } catch(InterruptedException e) {}
+      }
+      System.out.println(nombre+": "+Integer.toString(peso));
+      peso++;
+    }
+  }
+}
--- Tarea 3/src/Granja/main.java
+++ Tarea 3/src/Granja/main.java
@@ -1,11 +1,17 @@
 import java.util.*;
 
 public class  main {
+  static Vector<Cultivo> cultivos;
 
   public static void main(String[] args) {
     //Leer archivo
-
-    System.out.println("Hello World from main!");
+    Cultivo clt = new Cultivo("Thomotes",5,1);
+    //cultivos.add(new Cultivo("Thomotes",5,1));
+    //cultivos.lastElement().start();
+    clt.start();
+    try {
+      clt.join();
+    } catch(InterruptedException e) {}
   }
 
 }
